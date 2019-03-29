3bf34af3fc0cb7e02dd806021dd88333bd6d37ce
ozkanonur <39852038+onurrozkaan@users.noreply.github.com>
2019-03-29 15:52:35 +0300

    the lead actor

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const configuration = ( {
+  lenght: null,
+  possible: null
+} )
+
+const keygen = () => {
+
+  let result = ''
+
+  if ( configuration.lenght == null ) {
+    configuration.lenght = 7
+  }
+
+  if ( configuration.possible == null ) {
+    configuration.possible = 'Awxyz08EFGLmno345pqrstMDSTY12HIJKZabcNOPQRBCdef67ghijkUVWXluv9'
+  }
+
+  for ( let x = 0; x < configuration.lenght; x++ ) {
+    result += configuration.possible.charAt( Math.floor( Math.random() * configuration.possible.length ) )
+  }
+
+  return result
+}
+
+module.exports = {
+  configuration,
+  keygen
+}
