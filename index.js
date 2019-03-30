b2af6ed7074cd755497691ac71f60c8a0f7ff886
ozkanonur <39852038+onurrozkaan@users.noreply.github.com>
2019-03-30 21:25:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const configuration = ({
   lenght: null,
-  possibility: null
+  possibility: null,
+  case: 'mixed'
 })
 
 const keymachine = () => {
@@ -13,11 +14,20 @@ const keymachine = () => {
   }
 
   if (configuration.possibility == null) {
-    configuration.possibility = 'Awxyz08EFGLmno345pqrstMDSTY12HIJKZabcNOPQRBCdef67ghijkUVWXluv9'
+    configuration.possibility = 'abcdefghijklmnopqrstuvwxyz0123456789'
   }
 
   for (let x = 0; x < configuration.lenght; x++) {
-    result += configuration.possibility.charAt(Math.floor(Math.random() * configuration.possibility.length))
+    if (configuration.case === 'mixed') {
+      let chance = Math.random() < 0.5
+      result += (chance === true) ? configuration.possibility.toLowerCase().charAt(Math.floor(Math.random() * configuration.possibility.length)) : configuration.possibility.toUpperCase().charAt(Math.floor(Math.random() * configuration.possibility.length))
+    } else if (configuration.case === 'upper') {
+      result += configuration.possibility.toUpperCase().charAt(Math.floor(Math.random() * configuration.possibility.length))
+    } else if (configuration.case === 'lower') {
+      result += configuration.possibility.toLowerCase().charAt(Math.floor(Math.random() * configuration.possibility.length))
+    } else {
+      result = 'Invalid configurations. Please check usage -> https://github.com/ozkanonur/keymachine/blob/master/README.md'
+    }
   }
 
   return result
