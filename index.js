fac2d1fad5a573cb6d68fa20540a0e37a4d13f37
nimda <nimda@localhost.localdomain>
2020-08-09 18:09:46 +0300

    Refactor keymachine to be universal module

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-export const keymachine = (configuration) => {
-	const possibility = configuration?.possibility || 'abcdefghijklmnopqrstuvwxyz0123456789'
-	const length = configuration?.length || 24
-	const letterCase = configuration?.case || 'mixed'
+;(function(name, root, factory) {
+	if (typeof exports === 'object') {
+		module.exports = factory();
+		module.exports['default'] = factory();
+	} else if (typeof define === 'function' && define.amd) {
+		define(factory);
+	} else {
+		root[name] = factory();
+	}
+})('keymachine', this, function() {
+	function keymachine(_configuration) {
+		const configuration = _configuration || {}
+		const possibility = ('possibility' in configuration) ? configuration.possibility : 'abcdefghijklmnopqrstuvwxyz0123456789';
+		const length = ('length' in configuration) ? configuration.length : 24;
+		const letterCase = ('case' in configuration) ? configuration.case : 'mixed';
 
-	let result = ''
+		let result = '';
 
-	for (let x = 0; x < length; x++) {
-		if (letterCase === 'lower') {
-			result += possibility.toLowerCase().charAt(Math.floor(Math.random() * possibility.length))
+		for (let x = 0; x < length; x++) {
+			if (letterCase === 'lower') {
+				result += possibility.toLowerCase().charAt(Math.floor(Math.random() * possibility.length));
+			} else if (letterCase === 'upper') {
+				result += possibility.toUpperCase().charAt(Math.floor(Math.random() * possibility.length));
+			} else {
+				let chance = Math.random() < 0.5;
+				result +=
+					chance === true
+						? possibility.toLowerCase().charAt(Math.floor(Math.random() * possibility.length))
+						: possibility.toUpperCase().charAt(Math.floor(Math.random() * possibility.length));
+			}
 		}
 
-		else if (letterCase === 'upper') {
-			result += possibility.toUpperCase().charAt(Math.floor(Math.random() * possibility.length))
-		}
-
-		else {
-			let chance = Math.random() < 0.5
-			result += (chance === true) ?
-				possibility.toLowerCase().charAt(Math.floor(Math.random() * possibility.length))
-				: possibility.toUpperCase().charAt(Math.floor(Math.random() * possibility.length))
-		}
+		return result;
 	}
 
-	return result
-}
+	return keymachine;
+});
